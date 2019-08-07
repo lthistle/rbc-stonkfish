5226d16b81eb4c263d2775555ddbd9c6f388b819
Srikar Gouru <srikarg89@gmail.com>
2019-08-07 18:18:27 -0400

    Fixed some stuff

--- sample_bots/ghost_bot.py
+++ sample_bots/ghost_bot.py
@@ -16,7 +16,7 @@ class GhostBot(Player):
         # make sure stockfish environment variable exists
         if STOCKFISH_ENV_VAR not in os.environ:
             raise KeyError(
-                'TroutBot requires an environment variable called "{}" pointing to the Stockfish executable'.format(
+                'GhostBot requires an environment variable called "{}" pointing to the Stockfish executable'.format(
                     STOCKFISH_ENV_VAR))
 
         # make sure there is actually a file
@@ -40,7 +40,7 @@ class GhostBot(Player):
             self.board.remove_piece_at(capture_square)
             for state in self.states:
                 self.state.turn = self.opponent_color
-                attackers = state.attackers(color, capture_square):
+                attackers = state.attackers(color, capture_square)
                 for attacker in attackers:
                     temp = state.copy()
                     temp.set_piece_at(capture_square, attacker)        
@@ -62,14 +62,14 @@ class GhostBot(Player):
         for square in expected:
             table = {}
             for state in self.states:
-				piece = state.piece_at(square)
-				if piece.color == self.color:
-					continue
+                piece = state.piece_at(square)
+                if piece.color == self.color:
+                    continue
                 table[piece.piece_type] = 1 if piece.piece_type in table else table[piece.piece_type] + 1
 
             for piece in table:
-                p = table[piece] / len(self.states)
-                expected[square] += p * (len(self.states) - table[piece])
+                p = table[piece]/len(self.states)
+                expected[square] += p*(len(self.states) - table[piece])
 
         scan = np.argmax(scipy.signal.convolve2d(np.pad(expected.reshape(8, 8), ((1,)*2,)*2, 'constant'), np.array([1]*9).reshape(3, 3)))
         return scan
@@ -87,6 +87,7 @@ class GhostBot(Player):
         self.states = confirmed_states
 
     def choose_move(self, move_actions: List[chess.Move], seconds_left: float) -> Optional[chess.Move]:
+
         return random.choice(move_actions + [None])
 
     def handle_move_result(self, requested_move: Optional[chess.Move], taken_move: Optional[chess.Move],
@@ -99,17 +100,19 @@ class GhostBot(Player):
                 self.states = self.filter_states(lambda board: board.color_at(capture_square) == self.opponent_color)
             for state in self.states:
                 state.push(taken_move)
-		else if requested_move is not None:
+        elif requested_move is not None:
             #Request failed, so remove all the boards that allowed that move to occur
-			self.states = self.filter_states(lambda board: requested_move not in board.pseudo_legal_moves)
+            self.states = self.filter_states(lambda board: requested_move not in board.pseudo_legal_moves)
 
     def handle_game_end(self, winner_color: Optional[Color], win_reason: Optional[WinReason],
                         game_history: GameHistory):
         print("BET WE WON BOYS")
     
-	def filter_states(self, func):
-		filtered = []
-		for state in self.states:
-			if func(state):
-				filtered.append(state)
-		return filtered
+    def filter_states(self, func):
+        filtered = []
+        for state in self.states:
+            if func(state):
+                filtered.append(state)
+        return filtered
+
+bot = GhostBot()
