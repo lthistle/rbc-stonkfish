2fc779e4d4c1c3dd46e819105fb1b3efc489d54d
Stephen Huan <stephen03.huan@gmail.com>
2019-08-13 13:38:51 -0400

    kill existing connections before making new ones

--- tests/server.py
+++ tests/server.py
@@ -1,15 +1,23 @@
 import os, sys, subprocess, json
 
 subprocess.run(["tmux", "kill-server"])
-subprocess.run(["pkill", "rc-connect"])
 subprocess.run(["git", "pull"])
 
 bot = json.load(open("bots.json"))[sys.argv[1]]
+print(f"Loaded bot {bot['username']}")
+
 # create temp file becuase bash is stupid with the ` character
 with open("temp.sh", "w") as f:
+    f.write("export STOCKFISH_EXECUTABLE=~/stockfish-10-linux/Linux/stockfish_10_x64 \n")
+    f.write("pkill rc-connect \n")
     f.write(f"~/.local/bin/rc-connect --username {bot['username']} --password \"{bot['password']}\" {bot['path']} --max-concurrent-games 1")
 
 for computer in bot["computers"]:
-    subprocess.run(["tmux", "new-session", "-d", "-s", computer, f"ssh {computer} bash -s < temp.sh"])
+    if len(sys.argv) > 2 and sys.argv[2] == "kill":
+        print(f"Killing existing connections on {computer}")
+        subprocess.run(["ssh", computer, "pkill rc-connect"])
+    else:
+        print(f"Starting new connection on {computer}")
+        subprocess.run(["tmux", "new-session", "-d", "-s", computer, f"ssh {computer} bash -s < temp.sh"])
 
 os.remove("temp.sh")
